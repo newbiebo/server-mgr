1b9e55c6cb22b8059430812000446cf63c3022e6
newbiebo <1186644190wxb@gmail.com>
2023-06-19 17:06:43 +0800

    add:backup init runner

---
+++ src/main/java/org/simple/sm/backup/runner/BackupInitRunner.java
@@ -0,0 +1,19 @@
+package org.simple.sm.backup.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+/**
+ * @author wangbo
+ * init backup
+ */
+@Slf4j
+public class BackupInitRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+
+
+    }
+}
