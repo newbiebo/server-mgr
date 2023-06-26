601151a5fff52c9e85f95cf380fb6870f39575e2
wangbo <1186644190wxb@gmail.com>
2023-06-26 22:27:25 +0800

    modify: @MapperScan

--- src/main/java/org/simple/sm/ServerManagerApplication.java
+++ src/main/java/org/simple/sm/ServerManagerApplication.java
@@ -1,5 +1,6 @@
 package org.simple.sm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ServletComponentScan
 @SpringBootApplication
+@MapperScan("org/simple/sm/db/sqlite/mapper")
 public class ServerManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerManagerApplication.class, args);
