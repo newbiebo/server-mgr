c979c5aac4e07c2ea4f3975d3c2caf911ff2f419
newbiebo <1186644190wxb@gmail.com>
2023-06-28 14:11:09 +0800

    modify:backup support history record

--- src/main/java/org/simple/sm/backup/service/impl/BackupServiceImpl.java
+++ src/main/java/org/simple/sm/backup/service/impl/BackupServiceImpl.java
@@ -1,34 +1,55 @@
 package org.simple.sm.backup.service.impl;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.simple.sm.backup.dto.req.BackupManualReqDTO;
 import org.simple.sm.backup.service.BackupService;
+import org.simple.sm.common.constant.ENUM_BACKUP_TYPE;
+import org.simple.sm.db.sqlite.entity.TBackupHistory;
+import org.simple.sm.db.sqlite.service.TBackupHistoryService;
+import org.simple.sm.utils.SequenceUtil;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Date;
 
 @Slf4j
 @Service
 public class BackupServiceImpl implements BackupService {
 
+    @Resource
+    TBackupHistoryService tBackupHistoryService;
+
     @Override
     public void backupFiles(BackupManualReqDTO backupManualReqDTO) {
+        BaseMapper<TBackupHistory> baseMapper = tBackupHistoryService.getBaseMapper();
         //nio copy file
         Path destinationFolder = Paths.get(backupManualReqDTO.getTargetPath());
         backupManualReqDTO.getSourceFiles().forEach(e->{
             Path sourceFile = Paths.get(e);
             Path destinationFile = destinationFolder.resolve(sourceFile.getFileName());
             try {
+                //copy
                 Files.copy(sourceFile, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+                //persistent data
+                TBackupHistory tBackupHistory = new TBackupHistory();
+                tBackupHistory.setBackupHistoryNo(SequenceUtil.generateId());
+                tBackupHistory.setType(ENUM_BACKUP_TYPE.MANUAL.getCode());
+                tBackupHistory.setJobNo("default");
+                tBackupHistory.setTargetPath(backupManualReqDTO.getTargetPath());
+                tBackupHistory.setResourcePath(backupManualReqDTO.getSourceFiles().toString());
+                tBackupHistory.setGmtCreate(new Date());
+                tBackupHistory.setGmtModified(new Date());
+                baseMapper.insert(tBackupHistory);
             } catch (IOException ex) {
-                log.error("file copy filed! as {}",ex.getMessage());
+                log.error("An exception occurred while copying the file! as {}",ex.getMessage());
             }
         });
-        //history to db todo
     }
 
     @Override
--- src/main/java/org/simple/sm/common/constant/ENUM_BACKUP_TYPE.java
+++ src/main/java/org/simple/sm/common/constant/ENUM_BACKUP_TYPE.java
@@ -5,16 +5,16 @@ public enum ENUM_BACKUP_TYPE {
     MANUAL("MANUAL", "手动执行"),
     TASK("TASK", "任务执行");
 
-    private final String name;
+    private final String code;
     private final String desc;
 
-    ENUM_BACKUP_TYPE(String name, String desc) {
-        this.name = name;
+    ENUM_BACKUP_TYPE(String code, String desc) {
+        this.code = code;
         this.desc = desc;
     }
 
-    public String getName() {
-        return name;
+    public String getCode() {
+        return code;
     }
 
     public String getDesc() {
