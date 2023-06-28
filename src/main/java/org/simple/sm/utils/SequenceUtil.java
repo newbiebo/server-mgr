9b0e4c4b9da046c6997bc1b6118b0392cd275d5d
newbiebo <1186644190wxb@gmail.com>
2023-06-28 14:12:06 +0800

    add:Primary key generation tool

---
+++ src/main/java/org/simple/sm/utils/SequenceUtil.java
@@ -0,0 +1,15 @@
+package org.simple.sm.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class SequenceUtil {
+
+    // todo Later upgrade to Snowflake ID
+    public static String generateId(){
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+}
