ef5d4c6a530d1d8aa344f354d2812974b3423170
wangbo <1186644190wxb@gmail.com>
2023-07-19 21:01:03 +0800

    add: null

--- src/main/java/org/simple/sm/db/sqlite/mapper/ZJobMapper.java
+++ src/main/java/org/simple/sm/db/sqlite/mapper/ZJobMapper.java
@@ -1,13 +1,16 @@
 package org.simple.sm.db.sqlite.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.simple.sm.quartz.dto.job.ExecutableBackupJob;
+import org.simple.sm.quartz.dto.job.ExecutableBarkJob;
 import org.simple.sm.quartz.dto.job.ExecutableJob;
 
 import java.util.List;
 
 public interface ZJobMapper {
+
     List<ExecutableBackupJob> getBackupJobList();
 
-    List<ExecutableJob> getBarkJobList();
+    List<ExecutableBarkJob> getBarkJobList();
 
 }
--- src/main/java/org/simple/sm/quartz/dto/job/ExecutableBackupJob.java
+++ src/main/java/org/simple/sm/quartz/dto/job/ExecutableBackupJob.java
@@ -1,14 +1,11 @@
 package org.simple.sm.quartz.dto.job;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class ExecutableBackupJob extends ExecutableJob{
+
     private String resourcePath;
     private String targetPath;
-    /**
-     * execution task type
-     */
-    private String type;
+
 }
