f33ecb41ace43dcd6e5622eaf0270cc221b9a9bc
newbiebo <1186644190wxb@gmail.com>
2024-01-10 22:53:20 +0800

    optimize code

--- src/main/java/org/simple/sm/backup/service/BackupService.java
+++ src/main/java/org/simple/sm/backup/service/BackupService.java
@@ -3,12 +3,29 @@ package org.simple.sm.backup.service;
 import org.simple.sm.backup.dto.req.BackupManualReqDTO;
 import org.simple.sm.backup.dto.res.BackupManualResDTO;
 
+/**
+ * backupService
+ */
 public interface BackupService {
-    //Backup specified files
+    /**
+     * Backup specified files
+     * @param backupManualReqDTO
+     * @return
+     */
     BackupManualResDTO backupFiles(BackupManualReqDTO backupManualReqDTO);
-    //Backup all files in the path
+
+    /**
+     * Backup all files in the path
+     * @param backupManualReqDTO
+     * @return
+     */
     BackupManualResDTO backupPath(BackupManualReqDTO backupManualReqDTO);
-    //Compress before backup
+
+    /**
+     *  Compress before backup
+     * @param backupManualReqDTO
+     * @return
+     */
     BackupManualResDTO backupAfterCompress(BackupManualReqDTO backupManualReqDTO);
 
 }
