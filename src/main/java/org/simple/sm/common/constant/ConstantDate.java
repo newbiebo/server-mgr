0a440de721cee07bf643589857efca6cce7033ac
newbiebo <1186644190wxb@gmail.com>
2024-01-11 20:56:52 +0800

    fix:method compressFiles and compressFolder compression not effective

---
+++ src/main/java/org/simple/sm/common/constant/ConstantDate.java
@@ -0,0 +1,10 @@
+package org.simple.sm.common.constant;
+
+import lombok.Getter;
+
+@Getter
+public class ConstantDate {
+
+    public static final String yyyyMMddHHmmssSSS = "yyyyMMddHHmmssSSS";
+
+}
