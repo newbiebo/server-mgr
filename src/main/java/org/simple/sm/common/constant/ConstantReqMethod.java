9c6e99982408b5e38b6a87c8dc882ee0cf2fc448
newbiebo <1186644190wxb@gmail.com>
2024-01-17 15:25:29 +0800

    add: constant of request method

---
+++ src/main/java/org/simple/sm/common/constant/ConstantReqMethod.java
@@ -0,0 +1,9 @@
+package org.simple.sm.common.constant;
+
+import lombok.Getter;
+
+@Getter
+public class ConstantReqMethod {
+    public static final String POST = "POST";
+    public static final String GET = "GET";
+}
